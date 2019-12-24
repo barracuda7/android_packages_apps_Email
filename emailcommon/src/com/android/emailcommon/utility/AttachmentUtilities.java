78107a1b0411620d57143436b3599c6cc486bf3a
barracuda7 <barracudaking@gmail.com>
2019-12-25 10:44:41 +1100

    re sync

--- emailcommon/src/com/android/emailcommon/utility/AttachmentUtilities.java
+++ emailcommon/src/com/android/emailcommon/utility/AttachmentUtilities.java
@@ -111,8 +111,6 @@ public class AttachmentUtilities {
         "hta", "ins", "isp", "jse", "lib", "mde", "msc", "msp",
         "mst", "pif", "scr", "sct", "shb", "sys", "vb", "vbe",
         "vbs", "vxd", "wsc", "wsf", "wsh",
-        // File types of common compression/container formats (again, to avoid malware)
-        "zip", "gz", "z", "tar", "tgz", "bz2",
     };
     /**
      * Filename extensions of attachments that can be installed.
@@ -128,14 +126,14 @@ public class AttachmentUtilities {
      * so we should probably factor that in. A 5MB attachment will generally be around
      * 6.8MB downloaded but only 5MB saved.
      */
-    public static final int MAX_ATTACHMENT_DOWNLOAD_SIZE = (25 * 1024 * 1024);
+    public static final int MAX_ATTACHMENT_DOWNLOAD_SIZE = (5 * 1024 * 1024);
     /**
      * The maximum size of an attachment we're willing to upload (measured as stored on disk).
      * Attachments that are base64 encoded (most) will be about 1.375x their actual size
      * so we should probably factor that in. A 5MB attachment will generally be around
      * 6.8MB uploaded.
      */
-    public static final int MAX_ATTACHMENT_UPLOAD_SIZE = (25 * 1024 * 1024);
+    public static final int MAX_ATTACHMENT_UPLOAD_SIZE = (5 * 1024 * 1024);
 
     private static Uri sUri;
     public static Uri getAttachmentUri(long accountId, long id) {
